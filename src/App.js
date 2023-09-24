de905fe15136f7aa26bf0b3f400d3c06f2f2707b
Abdurrahman Qureshi <scythethroughtheheart05@gmail.com>
2023-09-24 10:27:55 +0530

    2.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Home from "./components/Home";
-import MySVG from "./components/MySVG";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <MySVG/>
           <Home/>
       </header>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,132 +1,134 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import styled from "styled-components";
 import Song from "./Song";
 import "../styles/styles.css";
 import Artist from "./Artist";
-import { motion } from "framer-motion";
-import { scaleUp } from "./Animations";
-
+import {motion} from "framer-motion";
+import {scaleUp} from "./Animations";
+// require('dotenv').config()
 const Home = () => {
-  const [searchBy, setSearchBy] = useState(false);
-  const [show, setShow] = useState(false);
-  const [data, setData] = useState([]);
-  const [artist, setArtist] = useState([]);
-  const fetchHandler = async () => {
-    let inputSong = document.getElementById("typed-song");
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const [searchBy, setSearchBy] = useState(false);
+    const [show, setShow] = useState(false);
+    const [data, setData] = useState([]);
+    const [artist, setArtist] = useState([]);
+    const fetchHandler = async () => {
+        let inputSong = document.getElementById("typed-song");
 
-    const optionsTitle = {
-      method: "GET",
-      url: "https://deezerdevs-deezer.p.rapidapi.com/search",
-      params: { q: `${inputSong.value}` },
-      headers: {
-        "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
-        "X-RapidAPI-Host": process.env.REACT_APP_API_URL,
-      },
+        const optionsTitle = {
+            method: "GET",
+            url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+            params: {q: `${inputSong.value}`},
+            headers: {
+                'X-RapidAPI-Key': `${apiKey}`,
+                'X-RapidAPI-Host': `${apiUrl}`
+            },
+        };
+        try {
+            const response = await axios.request(optionsTitle);
+            console.log(response.data);
+            if (inputSong.value === "") {
+                setShow(false);
+            } else {
+                setShow(true);
+            }
+            setData({
+                ...data,
+                artistCover: response.data.data[0].artist.picture_medium,
+                songCover: response.data.data[0].album.cover_big,
+                artistName: response.data.data[0].artist.name,
+                songName: response.data.data[0].title_short,
+                album: response.data.data[0].album.title,
+                songDuration: response.data.data[0].duration,
+                songPrev: response.data.data[0].preview,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+
+        const optionsArtist = {
+            method: "GET",
+            url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+            params: {q: `${inputSong.value}`},
+            headers: {
+                'X-RapidAPI-Key': `${apiKey}`,
+                'X-RapidAPI-Host': `${apiUrl}`
+            },
+        };
+
+        try {
+            const response = await axios.request(optionsArtist);
+            console.log(response.data);
+            const top10Artists = response.data.data.slice(0, 25);
+            console.log(top10Artists);
+            if (inputSong.value === "") {
+                setShow(false);
+            } else {
+                setShow(true);
+            }
+
+            setArtist(Array.from(top10Artists));
+        } catch (error) {
+            console.error(error);
+            console.log(process.env.REACT_APP_API_KEY);
+        }
     };
-    try {
-      const response = await axios.request(optionsTitle);
-      console.log(response.data);
-      if (inputSong.value === "") {
-        setShow(false);
-      } else {
-        setShow(true);
-      }
-      setData({
-        ...data,
-        artistCover: response.data.data[0].artist.picture_medium,
-        songCover: response.data.data[0].album.cover_big,
-        artistName: response.data.data[0].artist.name,
-        songName: response.data.data[0].title_short,
-        album: response.data.data[0].album.title,
-        songDuration: response.data.data[0].duration,
-        songPrev: response.data.data[0].preview,
-      });
-    } catch (error) {
-      console.error(error);
-    }
 
-    const optionsArtist = {
-      method: "GET",
-      url: "https://deezerdevs-deezer.p.rapidapi.com/search",
-      params: { q: `${inputSong.value}` },
-      headers: {
-        "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
-        "X-RapidAPI-Host": process.env.REACT_APP_API_URL,
-      },
+    const filterHandler = () => {
+        setSearchBy(!searchBy);
+        console.log(searchBy);
     };
-
-    try {
-      const response = await axios.request(optionsArtist);
-      console.log(response.data);
-      const top10Artists = response.data.data.slice(0, 25);
-      console.log(top10Artists);
-      if (inputSong.value === "") {
-        setShow(false);
-      } else {
-        setShow(true);
-      }
-
-      setArtist(Array.from(top10Artists));
-    } catch (error) {
-      console.error(error);
-      console.log(process.env.REACT_APP_API_KEY);
-    }
-  };
-
-  const filterHandler = () => {
-    setSearchBy(!searchBy);
-    console.log(searchBy);
-  };
-  const clearHandler = () => {
-    let inputSong = document.getElementById("typed-song");
-    inputSong.value = "";
-  };
-  return (
-    <StyledHome variants={scaleUp} initial="initial" animate="show">
-      <StyledTitle variants={scaleUp}>
-        R<span>h</span>y<span>t</span>h<span>m</span> R<span>e</span>v
-        <span>e</span>a<span>l</span>e<span>r</span>
-      </StyledTitle>
-      <StyledTop variants={scaleUp}>
-        <StyledInput
-          variants={scaleUp}
-          id="typed-song"
-          placeholder="Type here..."
-          type="text"
-        />
-        <StyledButtonDiv>
-          <StyledButton
-            variants={scaleUp}
-            className={searchBy ? "disabled" : "enabled"}
-            onClick={filterHandler}
-          >
-            By Artist
-          </StyledButton>
-          <StyledButton
-            variants={scaleUp}
-            className={searchBy ? "enabled" : "disabled"}
-            onClick={filterHandler}
-          >
-            By Title
-          </StyledButton>
-          <StyledButton variants={scaleUp} onClick={fetchHandler}>
-            Search
-          </StyledButton>
-          <StyledButton variants={scaleUp} onClick={clearHandler}>
-            Clear
-          </StyledButton>
-        </StyledButtonDiv>
-      </StyledTop>
-      <StyledBottom variants={scaleUp}>
-        {searchBy ? (
-          <Song data={data} show={show} />
-        ) : (
-          <Artist artist={artist} show={show} />
-        )}
-      </StyledBottom>
-    </StyledHome>
-  );
+    const clearHandler = () => {
+        let inputSong = document.getElementById("typed-song");
+        inputSong.value = "";
+    };
+    return (
+        <StyledHome variants={scaleUp} initial="initial" animate="show">
+            <StyledTitle variants={scaleUp}>
+                R<span>h</span>y<span>t</span>h<span>m</span> R<span>e</span>v
+                <span>e</span>a<span>l</span>e<span>r</span>
+            </StyledTitle>
+            <StyledTop variants={scaleUp}>
+                <StyledInput
+                    variants={scaleUp}
+                    id="typed-song"
+                    placeholder="Type here..."
+                    type="text"
+                />
+                <StyledButtonDiv>
+                    <StyledButton
+                        variants={scaleUp}
+                        className={searchBy ? "disabled" : "enabled"}
+                        onClick={filterHandler}
+                    >
+                        By Artist
+                    </StyledButton>
+                    <StyledButton
+                        variants={scaleUp}
+                        className={searchBy ? "enabled" : "disabled"}
+                        onClick={filterHandler}
+                    >
+                        By Title
+                    </StyledButton>
+                    <StyledButton variants={scaleUp} onClick={fetchHandler}>
+                        Search
+                    </StyledButton>
+                    <StyledButton variants={scaleUp} onClick={clearHandler}>
+                        Clear
+                    </StyledButton>
+                </StyledButtonDiv>
+            </StyledTop>
+            <StyledBottom variants={scaleUp}>
+                {searchBy ? (
+                    <Song data={data} show={show}/>
+                ) : (
+                    <Artist artist={artist} show={show}/>
+                )}
+            </StyledBottom>
+        </StyledHome>
+    );
 };
 
 const StyledHome = styled(motion.div)`
@@ -150,11 +152,13 @@ const StyledTitle = styled(motion.p)`
   letter-spacing: 15px;
   animation: opacityB infinite linear 3s;
   font-weight: bolder;
+
   span {
     animation: opacityA infinite linear 3s;
     color: #ff4081;
     font-weight: lighter;
   }
+
   @keyframes opacityA {
     0% {
       opacity: 1;
@@ -191,7 +195,7 @@ const StyledTitle = styled(motion.p)`
   }
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
     color: #1a1a1a;
-    span{
+    span {
       color: ghostwhite;
     }
   }
@@ -221,9 +225,11 @@ const StyledInput = styled(motion.input)`
   margin: 0px auto 1rem;
   text-align: center;
   font-family: "Nunito", sans-serif;
+
   &:focus {
     outline: none;
   }
+
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
     width: 90%;
   }
@@ -247,16 +253,20 @@ const StyledButton = styled(motion.div)`
   transition: 0.5s ease all;
   text-align: center;
   font-family: "Nunito", sans-serif;
+
   &:hover {
     animation: flash infinite linear 2s;
   }
+
   &.enabled {
     background-color: #ff4081;
     animation: flash infinite linear 2s;
   }
+
   &.disabled {
     background-color: #ff4081;
   }
+
   @keyframes flash {
     0% {
       background-color: #1a1a1a;
